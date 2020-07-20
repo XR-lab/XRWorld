8559cc640c970486def55c1584aeb855d9b81397
Richard Ram <richard@richard-ram.com>
2020-07-20 13:40:37 +0200

    Updated gitignore. Simple setup of reading levelData from JSON file.

---
+++ Assets/Scripts/LevelSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XRWorld.Database;
+
+namespace XRWorld.Gameplay
+{
+    public class LevelSpawner : MonoBehaviour
+    {
+        public void SpawnLevel(LevelData levelData)
+        {
+            foreach (var tileData in levelData.tiles)
+            {
+                GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                obj.transform.position = new Vector3(tileData.posX, 0, tileData.posZ);
+                obj.transform.SetParent(transform);
+            }
+        }
+    }
+}
+
