0b3f56b6fd7d82a561ee03e5a07f4068f2f51a49
Richard Ram <richard@richard-ram.com>
2020-07-20 16:58:30 +0200

    Added y-offset to spawning. Added placeableObject to spawn functionality.

---
+++ Assets/Scripts/Database/JSONParser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XRWorld.Core;
+
+namespace XRWorld.Database
+{
+    public class JSONParser : MonoBehaviour
+    {
+        public TextAsset database;
+
+        void Start()
+        {
+            LevelData data = JsonUtility.FromJson<LevelData>(database.ToString());
+
+            // TODO: fix direct reference to LevelSpawner
+            GetComponent<LevelSpawner>().SpawnLevel(data);
+        }
+    }
+}
+
