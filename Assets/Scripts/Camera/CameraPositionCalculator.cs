9b063524df75fa0c376614f1773a2ad7364bcd21
DragstylerNL <dragstylernl@gmail.com>
2020-08-10 15:25:55 +0200

    camera positions are done, now just the connection to the database and going from there

---
+++ Assets/Scripts/Camera/CameraPositionCalculator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using XRWorld.Database;
+
+namespace XRWorld.Core
+{
+    public class CameraPositionCalculator : MonoBehaviour
+    {
+        // whether or not Update should be active
+        private bool _active = false;
+        
+        // for calculating where the middle of the level is
+        [SerializeField] private Transform _levelSpawn;
+        [SerializeField] private Vector3 _offsetToTheMiddle;
+        private Vector3 _theMiddle;
+
+        private CameraChangeHandler _camHandler;
+        void Start()
+        {
+            _camHandler = FindObjectOfType<CameraChangeHandler>();
+        }
+
+        public void Setup()
+        {
+            _theMiddle = _levelSpawn.position + _offsetToTheMiddle;
+            _camHandler.AddCameraDataToDatabase();
+
+            _active = true;
+        }
+
+        void Update()
+        {
+            if(!_active) return;
+
+            Vector3 cameraOffsetToCenter = transform.position - _theMiddle;
+        }
+    }
+
+    public class JSONcamData()
+    {
+        public Vector3 position;
+
+        JSONcamData()
+        {
+            
+        }
+    }
+}
