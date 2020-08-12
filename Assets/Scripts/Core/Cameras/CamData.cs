283496abd4473bc6d204237e1eb3533ede95b7c1
DragstylerNL <dragstylernl@gmail.com>
2020-08-12 14:51:24 +0200

    camera rotation is now function enough :P

--- Assets/Scripts/Core/Cameras/CamData.cs
+++ Assets/Scripts/Core/Cameras/CamData.cs
@@ -16,9 +16,9 @@ namespace XRWorld.Core.Cameras
             this.pos = new Vector3Int(Mathf.FloorToInt(pos.x * 100),
                 Mathf.FloorToInt(pos.y * 100),
                 Mathf.FloorToInt(pos.z * 100));
-            this.rot = new Vector3Int((Mathf.FloorToInt(rot.x * 100)) / 100,
-                (Mathf.FloorToInt(rot.y * 100)) / 100,
-                (Mathf.FloorToInt(rot.z * 100)) / 100);
+            this.rot = new Vector3Int((Mathf.FloorToInt(rot.x * 1000)) / 1000,
+                (Mathf.FloorToInt(rot.y * 1000)) / 1000,
+                (Mathf.FloorToInt(rot.z * 1000)) / 1000);
         }
     }
 }
--- Assets/Scripts/Core/Cameras/CameraCollection.cs
+++ Assets/Scripts/Core/Cameras/CameraCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace XRWorld.Core.Cameras
@@ -18,8 +19,7 @@ namespace XRWorld.Core.Cameras
         
         public void CameraUpdate(CamData data)
         {
-            if(data.name == _sessionID) 
-                return;
+            //if(data.name == _sessionID) return;
 
             if (!_camCollection.ContainsKey(data.name))
             {
@@ -38,8 +38,46 @@ namespace XRWorld.Core.Cameras
         private void MoveCam(CamData data)
         {
             Vector3 pos = new Vector3(data.pos.x, data.pos.y,data.pos.z) / 100;
-            _camCollection[data.name].position = pos + _theMiddle;
+            StartCoroutine(CamLerp(pos + _theMiddle, data.rot, _camCollection[data.name]));
+        }
+
+        private float amountOfSteps = 10;
+        IEnumerator CamLerp(Vector3 targetPos, Vector3 targetRot, Transform camTransform)
+        {
+            Vector3 posSteps = (camTransform.position - targetPos) / amountOfSteps;
+           
+            Vector3 camRot = camTransform.rotation.eulerAngles;
+            Vector3 rotSteps = Vector3.zero;
+            
+            //TODO try to get this to work without summoning a devil that will trow up when it looks at this part of the code
+            rotSteps = -((camRot - targetRot) / amountOfSteps);
+            if(rotSteps.x > 18 || rotSteps.x < -18){rotSteps.x = (camRot.x>targetRot.x? -((camRot.x-360) - targetRot.x) : -(camRot.x - (targetRot.x-360))) / amountOfSteps;}
+            if(rotSteps.y > 18 || rotSteps.y < -18){rotSteps.y = (camRot.y>targetRot.y? -((camRot.y-360) - targetRot.y) : -(camRot.y - (targetRot.y-360))) / amountOfSteps;}
+            if(rotSteps.z > 18 || rotSteps.z < -18){rotSteps.z = (camRot.z>targetRot.z? -((camRot.z-360) - targetRot.z) : -(camRot.z - (targetRot.z-360))) / amountOfSteps;}
+            
+            print(camTransform.rotation.eulerAngles);
+            print(targetRot);
+            print(rotSteps);
+            
+            int i = 0;
+            while (i < amountOfSteps)
+            {
+                camTransform.position -= posSteps;
+                camTransform.Rotate(rotSteps);
+                
+                i++;
+                yield return new WaitForSeconds(0.5f / amountOfSteps);
+            }
+            yield return null;
+            camTransform.rotation = Quaternion.Euler(targetRot);
         }
     }
 }
 
+// Quaternion qRotSteps = new Quaternion(
+//     camRot.x > qTargetRot.x ? -(camRot.x - qTargetRot.x) / amountOfSteps : (qTargetRot.x - camRot.x) / amountOfSteps,
+//     camRot.y > qTargetRot.y ? -(camRot.y - qTargetRot.y) / amountOfSteps : (qTargetRot.y - camRot.y) / amountOfSteps,
+//     camRot.z > qTargetRot.z ? -(camRot.z - qTargetRot.z) / amountOfSteps : (qTargetRot.z - camRot.z) / amountOfSteps,
+//     camRot.w > qTargetRot.w ? -(camRot.w - qTargetRot.w) / amountOfSteps : (qTargetRot.w - camRot.w) / amountOfSteps);
+//    (camRot.w - qTargetRot.w) / amountOfSteps);
+//Vector3 rotSteps = -((camTransform.rotation.eulerAngles - TargetRot) / amountOfSteps);
