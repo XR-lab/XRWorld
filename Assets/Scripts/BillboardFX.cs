131a43cdf87ab35d380c7aef9464413c40a6cab8
Richard Ram <richard@richard-ram.com>
2020-08-18 16:33:54 +0200

    Update to billboarding and ui placement

--- Assets/Scripts/BillboardFX.cs
+++ Assets/Scripts/BillboardFX.cs
@@ -4,17 +4,8 @@ using UnityEngine;
 
 public class BillboardFX : MonoBehaviour
 {
-	public Transform camTransform;
-
-	Quaternion originalRotation;
-
-    void Start()
-    {
-        originalRotation = transform.rotation;
-    }
-
     void Update()
     {
-     	transform.rotation = camTransform.rotation * originalRotation;   
+	    transform.LookAt(Camera.main.transform);
     }
 }
